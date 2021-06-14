57e0d1a0acfd8c6b9825b0b46ed9f44b5ac0db02
AMJ <isaelwebdeveloper@gmail.com>
2021-06-14 13:47:02 -0600

    created buttons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,15 @@ export default function App(props) {
 		<main>
 			<div className="main-container">
 				<Header />
+
+                <article className='call-to-action-main'>
+                    <h1>LANDSCAPING SERVICES</h1>
+                    <p>We create a cheerful & pleasant ambience</p>
+                    <div className='call-to-action-main-buttons'>
+                        <button>MORE INFO</button>
+                        <button>CALL US</button>
+                    </div>
+                </article>
 			</div>
 		</main>
 	);
