59eca9887c0935f86899b119075a9edc2504628c
isael lizama <isaellizama@isaels-MacBook-Air.local>
2021-06-18 14:49:39 -0600

    finished first pass of contact form styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 const Contact = (props) => {
     return (
+    <section className='contact-container'>
+        <h1 className='contact-title'>Contact Us</h1>
+        <p className='contact-message'>Call or Message Us for a Free Quote!</p>
         <form className='contact-form'>
-            <input placeholder='Email' type='email' />
+            <input className='contact-email' placeholder='Email' type='email' />
             <input placeholder='Name' type='text' />
-            <input placeholder='Phone Number' type='text' />
-            <textarea value='' placeholder='Comments or Questions?'></textarea>
+            <input className='contact-phone' placeholder='Phone Number' type='text' />
+            <textarea rows='5' className='contact-comments' value='' placeholder='Comments or Questions?'></textarea>
+            <button className='contact-button'>Send!</button>
         </form>
+    </section>
     );
 };
 
