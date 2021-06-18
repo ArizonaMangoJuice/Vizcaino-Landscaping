cf9f8214e416490a07c47300ed539aba73eef894
isael lizama <isaellizama@isaels-MacBook-Air.local>
2021-06-18 12:10:32 -0600

    added stock images to to cards until client gives photoos

--- src/static/data.js
+++ src/static/data.js
@@ -4,8 +4,26 @@ const data = {
             title: 'Lawn Care Maintenance',
             desc: 'Transform your lawn',
             duration: '1 hr',
-            mainImage: 'https://static.wixstatic.com/media/d99169_786faa15e4af47659db8e9d2e9bc1037~mv2_d_5656_3933_s_4_2.jpg/v1/fill/w_3920,h_2424,al_c,q_90,usm_0.66_1.00_0.01/d99169_786faa15e4af47659db8e9d2e9bc1037~mv2_d_5656_3933_s_4_2.webp',
-        }
+            mainImage: 'https://images.unsplash.com/photo-1564944817179-f03792efda53?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80',
+        },
+        {
+            title: 'Fences',
+            desc: 'Get a quote with a consultation call!',
+            duration: '1 hr',
+            mainImage: 'https://static.wixstatic.com/media/abcf46_eab9c94c2863487b9289207ff5738af8~mv2.jpg/v1/fill/w_960,h_594,al_c,q_85/abcf46_eab9c94c2863487b9289207ff5738af8~mv2.webp',
+        },
+        {
+            title: 'Snow Cleaning',
+            desc: 'Call for our competitive prices!',
+            duration: '1-2 hr',
+            mainImage: 'https://images.unsplash.com/photo-1483385573908-0a2108937c4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=716&q=80',
+        },
+        {
+            title: 'House Decks',
+            desc: 'Transform your backyard!',
+            duration: '1-7 days',
+            mainImage: 'https://www.houselogic.com/wp-content/uploads/2009/11/deck-wood-diy-ufp_5a711a793e5a6ad3cde380304663fc06.jpg?w=570',
+        },
     ],
 };
 
