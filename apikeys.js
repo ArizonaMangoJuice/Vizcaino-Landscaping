ecbbad6a50cd86d8becf4d04fd6d91e8e6ede178
isael lizama <isaellizama@isaels-MacBook-Air.local>
2021-06-24 10:25:36 -0600

    added process.env

---
+++ apikeys.js
@@ -0,0 +1,3 @@
+export const USER_ID = process.env.USER_ID || '';
+export const ACCESS_TOKEN = process.env.ACCESS_TOKEN || '';
+export const TEMPLATE_ID = process.env.TEMPLATE_ID  || '';
---
+++ src/apikeys.js
@@ -0,0 +1,3 @@
+export const USER_ID = 'user_5GuZlAvUb1KzN8aDddcrM';
+export const ACCESS_TOKEN = 'dda13af7be2b5b50ca0f6518812ab1cd';
+export const TEMPLATE_ID = 'template_nfedatc';
