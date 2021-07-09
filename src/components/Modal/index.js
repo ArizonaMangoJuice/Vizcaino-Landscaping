01774b46682d9191c654213b37fdd876ea7367d5
isael lizama <isaellizama@isaels-MacBook-Air.local>
2021-07-09 07:50:23 -0600

    added next and prev to gallery

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Modal = ({ closeModal, images, resetImages }) => {
+    const [currentImage, setCurrentImage] = useState(0);
+
+    const nextImage = () => {
+        currentImage === images.length - 1 ? setCurrentImage(0) : setCurrentImage(num => num + 1);
+    }
+
+    const prevImage = () => {
+        currentImage === 0 ? setCurrentImage(images.length - 1) : setCurrentImage(num => num - 1);
+    }
     // const
     return (
         <section className='gallery-modal'>
             <article className='modal-picture-container'>
-                <button onClick className='modal-before'>&lt;</button>
-                <img alt='house landscaping' src={require('../../media/' + images[0]).default} className='modal-current-image' />
+                <button onClick={() => prevImage()} className='modal-before'>&lt;</button>
+                <img alt='house landscaping' src={require('../../media/' + images[currentImage]).default} className='modal-current-image' />
                 <button onClick={() => closeModal()} className='modal-close-modal'>X</button>
-                <button className='modal-before'>&gt;</button>
+                <button onClick={() => nextImage()} className='modal-before'>&gt;</button>
             </article>
         </section>
     )
